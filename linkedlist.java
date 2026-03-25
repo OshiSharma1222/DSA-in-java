c16ebc2d949ddb37a08d6654df86334ecd5bb69a
oshisharma1222 <oshisharma2609@gmail.com>
2026-03-25 22:42:19 +0530

    linked list concept

---
+++ linkedlist.java
@@ -0,0 +1,38 @@
+class Node{
+    private int data;
+    private Node next;//pointer to the next node
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+    public int getData(){
+        return data;
+    }
+    public void setData(int data){
+        this.data = data;
+
+    }
+    public Node getNext(){//Node use to point to the next node
+        return next;
+    }
+    public void setNext(Node next){
+        this.next = next;
+    }
+}
+public class linkedlist{
+    public static void main(String[] args) {
+        Node ten = new Node(10);
+        Node twenty = new Node(20);
+        Node head = ten;
+        ten.setNext(twenty);
+        twenty.setNext(null);
+        printlinkedlist(head);
+    }
+    public static void printlinkedlist(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.println(curr.getData());
+            curr = curr.getNext();
+        }
+        }
+    }
